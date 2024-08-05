e4013b1815031ba838c7e12f4bfadfc564c885ab
MaratXcode <kosjachok8@yandex.ru>
2024-08-06 01:18:46 +0300

    Initial Commit

---
+++ ColorizedViewSwiftUI/ColorizedViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedViewSwiftUIApp.swift
+//  ColorizedViewSwiftUI
+//
+//  Created by Marat Fakhrizhanov on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
